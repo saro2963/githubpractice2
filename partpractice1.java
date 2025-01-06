3384259550ab897835a068b83b194bd2bbc70ac7
saro2963 <saravanan.k2963@gmail.com>
2025-01-06 13:31:23 +0530

    java program coded

---
+++ partpractice1.java
@@ -0,0 +1,19 @@
+package githubpractice2;
+import java.util.Scanner;
+
+public class partpractice1
+{
+    public static void main(String args[])
+    {
+        Scanner o = new Scanner(System.in);
+        int a = o.nextInt();
+        for(int i=0; i<=a; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+   
+
+
+}
