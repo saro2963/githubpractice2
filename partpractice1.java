d285fad3ac9277c8229a29f3f373b8878cbb3d48
saro2963 <saravanan.k2963@gmail.com>
2025-01-08 00:46:39 +0530

    added new loop

--- partpractice1.java
+++ partpractice1.java
@@ -7,10 +7,17 @@ public class partpractice1
     {
         Scanner o = new Scanner(System.in);
         int a = o.nextInt();
+        o.nextLine();
+        String name = o.nextLine();
         for(int i=0; i<=a; i++)
         {
             System.out.println(i);
         }
+        for(int i=1;i<a;i++)
+        {
+            System.out.println(name);
+        }
+    
 
     }
    
